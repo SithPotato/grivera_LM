b7eff2940dae1c7c10ecca28117620b514c1da98
SithPotato <Kshinobi796@gmail.com>
2024-04-08 08:59:23 -0400

    update file

--- EjemploC#/obj/Debug/net8.0/EjemploC#.AssemblyInfo.cs
+++ EjemploC#/obj/Debug/net8.0/EjemploC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjemploC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b73f90b536365ddf8e961c1986d50e8b33ffbc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b7f09b44cebf773cc0b66732ac1dbbf9863c55d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjemploC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjemploC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
