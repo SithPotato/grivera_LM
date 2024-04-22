02bee88a182aa52d91195e73c062affa1b289fcb
SithPotato <Kshinobi796@gmail.com>
2024-04-22 09:00:11 -0400

    Update file

--- Laboratorios C++/Ejemplovg.cpp
+++ Laboratorios C++/Ejemplovg.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 using namespace std;
 
+        //Declaramos variables globales
 int numero01=10;
 int numero02=5;
-
+        //Declaracion de funciones
 int sumar();
 int restar();
-
+        //Definicion de Declaracion
 int main()
 {
     cout <<"Suma de los numeros:" <<sumar() <<endl;
     cout <<"Resta de los numeros:" <<restar() <<endl;
 }
 
-
+        //Definicion de funciones
 int sumar()
 {
     return numero01 + numero02;
